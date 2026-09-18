bc5fb9eb9e65c3d7a9de37e33e38a17fa665fd41
Noah, Laptop <noahchamberlainclark@gmal.com>
2026-09-18 00:03:55 -0400

    better replay number

--- game.py
+++ game.py
@@ -79,7 +79,7 @@ def trainingSequence(TABLE, net=NuralNet(16,make()[1]), logQueue=None, id=-1, tr
 	with open(f"replays.pkl", "wb") as f:
 		pickle.dump(replays, f)
 
-	loopNumb = min(len(replays), 100)
+	loopNumb = min(len(replays), 25)
 	for i in range(loopNumb):
 		thisTable=replays.pop(random.randint(0, len(replays)-1))
 		net.train(getTargs(thisTable, trainingStage))
