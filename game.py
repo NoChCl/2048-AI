04f01dd0b846aac66d708cd659989841a5711cb0
Noah, Laptop <noahchamberlainclark@gmal.com>
2026-09-18 18:36:28 -0400

    less bugs for realz

--- game.py
+++ game.py
@@ -79,7 +79,12 @@ def trainingSequence(TABLE, net=NuralNet(16,make()[1]), logQueue=None, id=-1, tr
 	with open(f"replays.pkl", "wb") as f:
 		pickle.dump(replays, f)
 
-	for thisTable in random.sample(replays, min(len(replays), 25)):
+	loopNumb = min(len(replays), 25)
+
+	for _ in range(loopNumb):
+		index = random.randrange(len(replays))
+		thisTable = replays.pop(index)
+
 		n, net = netInput(net, thisTable)
 		net.train(getTargs(thisTable, trainingStage))
 
