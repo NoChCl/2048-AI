7e50388553f32937033ee8d7ca01f848bfe3369c
Noah, Laptop <noahchamberlainclark@gmal.com>
2026-09-18 18:38:00 -0400

    revert

--- game.py
+++ game.py
@@ -80,11 +80,8 @@ def trainingSequence(TABLE, net=NuralNet(16,make()[1]), logQueue=None, id=-1, tr
 		pickle.dump(replays, f)
 
 	loopNumb = min(len(replays), 25)
-
-	for _ in range(loopNumb):
-		index = random.randrange(len(replays))
-		thisTable = replays.pop(index)
-
+	for i in range(loopNumb):
+		thisTable=replays.pop(random.randint(0, len(replays)-1))
 		n, net = netInput(net, thisTable)
 		net.train(getTargs(thisTable, trainingStage))
 
